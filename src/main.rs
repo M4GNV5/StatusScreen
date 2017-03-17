f7604d407b2a57056c0b92b2d3de348575a4aa2d
M4GNV5 <jakob@m4gnus.de>
2017-03-17 21:27:24 +0100

    move graph rendering to a function

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,37 @@ macro_rules! color
 	($r:expr, $g:expr, $b:expr) => { Color::RGBA($r, $g, $b, 255); }
 }
 
+fn draw_graph<F>(renderer: &Renderer, pos: Rect, color: Color, count: i16, func: F)
+	where F: Fn(i16) -> i16
+{
+	let x = pos.x() as i16;
+	let y = pos.y() as i16;
+	let width = pos.width() as i16;
+	let height = pos.height() as i16;
+
+	let mut last = func(0);
+	for i in 1..(count - 1)
+	{
+		let i = i as i16;
+		let val = func(i);
+		if val <= 0
+		{
+			continue;
+		}
+
+		renderer.thick_line(x + (i - 1) * width / count,
+			y + height - last,
+			x + i * width / count,
+			y + height - val,
+			2, color).unwrap();
+
+		last = val;
+	}
+
+	renderer.thick_line(x, y, x, y + height, 2, color!(0, 0, 0)).unwrap();
+	renderer.thick_line(x, y + height, x + width, y + height, 2, color!(0, 0, 0)).unwrap();
+}
+
 fn main()
 {
 	let mut pos = 0;
@@ -54,92 +85,65 @@ fn main()
 		renderer.set_draw_color(color!(255, 255, 255));
 		renderer.clear();
 
-		renderer.thick_line(10, 10, 10, 110, 2, color!(0, 0, 0)).unwrap();
-		renderer.thick_line(10, 110, 110, 110, 2, color!(0, 0, 0)).unwrap();
-		renderer.thick_line(120, 10, 120, 110, 2, color!(0, 0, 0)).unwrap();
-		renderer.thick_line(120, 110, 220, 110, 2, color!(0, 0, 0)).unwrap();
-
-		renderer.thick_line(10, 120, 10, 220, 2, color!(0, 0, 0)).unwrap();
-		renderer.thick_line(10, 220, 110, 220, 2, color!(0, 0, 0)).unwrap();
-		renderer.thick_line(120, 120, 120, 220, 2, color!(0, 0, 0)).unwrap();
-		renderer.thick_line(120, 220, 220, 220, 2, color!(0, 0, 0)).unwrap();
-
-		let mut cpu_last_usage: i64 = -1;
-		let mut mem_last_hardusage: i64 = -1;
-		let mut mem_last_usage: i64 = -1;
-		for i in 1 .. DATA_COUNT
 		{
-			let curr = (pos + i) % DATA_COUNT;
+			let surface = font.render("CPU").blended(color!(0, 0, 0)).unwrap();
+			let mut texture = renderer.create_texture_from_surface(&surface).unwrap();
+			let TextureQuery { width, height, .. } = texture.query();
 
-			{
-				let surface = font.render("CPU").blended(color!(0, 0, 0)).unwrap();
-				let mut texture = renderer.create_texture_from_surface(&surface).unwrap();
-				let TextureQuery { width, height, .. } = texture.query();
+			let pos1 = Rect::new(13, 10, width, height);
+			renderer.copy(&mut texture, None, Some(pos1)).unwrap();
 
-				let pos1 = Rect::new(13, 10, width, height);
-				renderer.copy(&mut texture, None, Some(pos1)).unwrap();
-
-				let pos2 = Rect::new(13, 120, width, height);
-				renderer.copy(&mut texture, None, Some(pos2)).unwrap();
-			}
-			{
-				let surface = font.render("MEM").blended(color!(0, 0, 0)).unwrap();
-				let mut texture = renderer.create_texture_from_surface(&surface).unwrap();
-				let TextureQuery { width, height, .. } = texture.query();
-
-				let pos1 = Rect::new(123, 10, width, height);
-				renderer.copy(&mut texture, None, Some(pos1)).unwrap();
-
-				let pos2 = Rect::new(123, 120, width, height);
-				renderer.copy(&mut texture, None, Some(pos2)).unwrap();
-			}
-
-			{
-				let next = (curr + 1) % DATA_COUNT;
-				let total_diff = cpu_history[next].total() as i64 - cpu_history[curr].total() as i64;
-				let idle_diff = cpu_history[next].idle() as i64 - cpu_history[curr].idle() as i64;
-
-				if total_diff != 0
-				{
-					let usage = ((total_diff as f64 - idle_diff as f64) / total_diff as f64 * 100f64) as i64;
-
-					if cpu_last_usage != -1
-					{
-						renderer.thick_line((11 + (i - 1) * 100 / DATA_COUNT) as i16,
-							(110 - cpu_last_usage) as i16,
-							(11 + i * 100 / DATA_COUNT) as i16,
-							(110 - usage) as i16,
-							2, color!(0, 0, 255)).unwrap();
-					}
-					cpu_last_usage = usage;
-				}
-			}
-
-			{
-				if mem_history[curr].total != 0
-				{
-					let hardused: i64 = (100 - mem_history[curr].available * 100 / mem_history[curr].total) as i64;
-					let used: i64 = (100 - mem_history[curr].free * 100 / mem_history[curr].total) as i64;
-
-					if mem_last_usage != -1
-					{
-						renderer.thick_line((121 + (i - 1) * 100 / DATA_COUNT) as i16,
-							(110 - mem_last_hardusage) as i16,
-							(121 + i * 100 / DATA_COUNT) as i16,
-							(110 - hardused) as i16,
-							2, color!(0, 255, 0)).unwrap();
-
-						renderer.thick_line((121 + (i - 1) * 100 / DATA_COUNT) as i16,
-							(110 - mem_last_usage) as i16,
-							(121 + i * 100 / DATA_COUNT) as i16,
-							(110 - used) as i16,
-							2, color!(255, 192, 0)).unwrap();
-					}
-					mem_last_hardusage = hardused;
-					mem_last_usage = used;
-				}
-			}
+			let pos2 = Rect::new(13, 120, width, height);
+			renderer.copy(&mut texture, None, Some(pos2)).unwrap();
 		}
+		{
+			let surface = font.render("MEM").blended(color!(0, 0, 0)).unwrap();
+			let mut texture = renderer.create_texture_from_surface(&surface).unwrap();
+			let TextureQuery { width, height, .. } = texture.query();
+
+			let pos1 = Rect::new(123, 10, width, height);
+			renderer.copy(&mut texture, None, Some(pos1)).unwrap();
+
+			let pos2 = Rect::new(123, 120, width, height);
+			renderer.copy(&mut texture, None, Some(pos2)).unwrap();
+		}
+
+		draw_graph(&renderer, Rect::new(10, 10, 100, 100), color!(0, 0, 255), DATA_COUNT as i16, |i| {
+			let next = (i as usize + pos + 1) % DATA_COUNT;
+			let curr = (i as usize + pos) % DATA_COUNT;
+			let total_diff = cpu_history[next].total() as i64 - cpu_history[curr].total() as i64;
+
+			if total_diff == 0
+			{
+				return -1;
+			}
+
+			let idle_diff = cpu_history[next].idle() as i64 - cpu_history[curr].idle() as i64;
+			return ((total_diff as f64 - idle_diff as f64) / total_diff as f64 * 100f64) as i16;
+		});
+
+		draw_graph(&renderer, Rect::new(120, 10, 100, 100), color!(255, 192, 0), DATA_COUNT as i16, |i| {
+			let curr = (i as usize + pos) % DATA_COUNT;
+			if(mem_history[curr].total == 0)
+			{
+				return -1;
+			}
+
+			return (100 - mem_history[curr].free * 100 / mem_history[curr].total) as i16;
+		});
+		draw_graph(&renderer, Rect::new(120, 10, 100, 100), color!(0, 255, 0), DATA_COUNT as i16, |i| {
+			let curr = (i as usize + pos) % DATA_COUNT;
+			if(mem_history[curr].total == 0)
+			{
+				return -1;
+			}
+
+		 	return (100 - mem_history[curr].available * 100 / mem_history[curr].total) as i16;
+		});
+
+		//TODO remote statistics etc.
+		draw_graph(&renderer, Rect::new(10, 120, 100, 100), color!(0, 0, 0), 0, |i| -1);
+		draw_graph(&renderer, Rect::new(120, 120, 100, 100), color!(0, 0, 0), 0, |i| -1);
 
 		renderer.present();
 
